a0f2a47f3b7391d2b1af24b68393b4dc07afbfcf
adriandelgg <adriandelgadoc1@gmail.com>
2024-10-06 16:44:52 -0500

    Removed src folder & configured tailwind w/ preline

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { WorkExperience } from "@/app/components/work-experience/WorkExperience";
+
+export default function Home() {
+  return (
+    <main>
+      <WorkExperience />
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/preline/preline.js",
+    "./app/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  plugins: [require("preline/plugin")],
+  plugins: [require("preline/plugin")]
 };
 
 export default config;
